67848f0b8ad20042eba79b64a39fe93b6cdf444e
ikurakorogaru <harusame.hamusame@gmail.com>
2026-04-26 21:10:03 +0900

    actionwheel改良
    head内をeye,rotateに分割

--- script/manager/actionwheel.lua
+++ script/manager/actionwheel.lua
@@ -16,31 +16,37 @@ local function reloadActions(openpage)
 	aw.setnum("pages", nil, {})
 	aw.setnum("pages", "root", action_wheel:newPage())
 	local pages = aw.getnum("pages", nil)
+	--head
 	aw.newActionPage(pages.root, "head", "heads", "minecraft:player_head", 255, 245, 169, true)
-	aw.newToggleAction(pages.head, "head", "headToggle", "minecraft:lever", true, 255, 245, 169, true)
-	aw.newNumAction(pages.head, "headSpeedChange", "headSpeedChange", "minecraft:lever", 1, 255, 245, 169,
+	--head\rotate
+	aw.newActionPage(pages.head, "rotate", "rotates", "minecraft:lever", 128, 128, 128, true)
+	aw.newToggleAction(pages.rotate, "head", "headToggle", "minecraft:lever", true, 255, 245, 169, true)
+	aw.newNumAction(pages.rotate, "headSpeedChange", "headSpeedChange", "minecraft:lever", 1, 255, 245, 169,
 		true)
-	aw.newAction(pages.head, "headSpeedReset", "headSpeedReset", "minecraft:barrier", pings.speedReset,
+	aw.newAction(pages.rotate, "headSpeedReset", "headSpeedReset", "minecraft:barrier", pings.speedReset,
 		nil,
 		255, 245, 169, true)
-	aw.newToggleAction(pages.head, "headSpeed", "headSpeedToggle", "minecraft:lever", true, 255, 245, 169, true)
-	aw.newAction(pages.head, "blink", "blink", "minecraft:oak_button", pings.blink, nil, 255, 245, 169, true)
-	aw.newAction(pages.head, "wink", "wink", "minecraft:oak_button", pings.lwink, pings.rwink, 255, 245, 169, true)
-	aw.newToggleAction(pages.head, "autoBlink", "autoBlinkToggle", "minecraft:lever", true, 255, 245, 169, true)
-
+	aw.newToggleAction(pages.rotate, "headSpeed", "headSpeedToggle", "minecraft:lever", true, 255, 245, 169, true)
+	--head\eye
+	aw.newActionPage(pages.head, "eye", "eyes", "minecraft:ender_eye", 0, 89, 36, true)
+	aw.newAction(pages.eye, "blink", "blink", "minecraft:oak_button", pings.blink, nil, 255, 245, 169, true)
+	aw.newAction(pages.eye, "wink", "wink", "minecraft:oak_button", pings.lwink, pings.rwink, 255, 245, 169, true)
+	aw.newToggleAction(pages.eye, "autoBlink", "autoBlinkToggle", "minecraft:lever", true, 255, 245, 169, true)
+	--name
 	aw.newActionPage(pages.root, "name", "names", "minecraft:name_tag", 33, 33, 33, true)
 	aw.newToggleAction(pages.name, "namerainbow", "nametagToggle", "minecraft:lever", false, 255, 245, 169, true)
 	aw.newToggleAction(pages.name, "namerainbowmove", "nametagMove", "minecraft:lever", false, 255, 245, 169, true)
 	aw.newToggleAction(pages.name, "afk", "afkToggle", "minecraft:barrier", false, 255, 0, 0, true)
-
+	--animation
 	aw.newActionPage(pages.root, "animation", "animations", "minecraft:observer", 91, 125, 66, true)
 	aw.newAction(pages.animation, "animation_1", "animation 1", "minecraft:oak_button", pings.animation_1, nil, 42, 42, 42,
 		true)
-
+	--other
 	aw.newActionPage(pages.root, "other", "others", "minecraft:slime_block", 0, 192, 0, true)
 	aw.newAction(pages.other, "effect", "effect", "minecraft:oak_button", pings.effect, nil, 255, 245, 169, true)
 	aw.newAction(pages.other, "armorvisible", "armorvisible", "minecraft:oak_button", pings.avisible, pings.aunvisible,
 		255, 245, 169, true)
+	--test
 	aw.newActionPage(pages.root, "test", "tests", "minecraft:command_block", 0, 0, 0, true)
 	aw.newActionPage(pages.test, "test_test", "tests", "minecraft:command_block", 0, 0, 0, true)
 	if openpage ~= nil then
