b2882a42124a879a2d1e4c39171ed581c04a5466
ikurakorogaru <harusame.hamusame@gmail.com>
2026-05-02 09:02:55 +0900

    定期同期
    ping.luaに

--- script/ping.lua
+++ script/ping.lua
@@ -5,6 +5,8 @@ local update_order = {}
 local handlers = {}
 local t = 0
 local send_limit = 16
+local full_sync_interval = 600
+local full_sync_index = 1
 
 function p.setnum(name, num, update_type)
 	nums[name] = num
@@ -39,9 +41,9 @@ function pings.innums(name, num)
 end
 
 function events.tick()
-	if t % 30 == 0 then
-		local sent = 0
+	local sent = 0
 
+	if t % 30 == 0 then
 		while #update_order > 0 and sent < send_limit do
 			local name = table.remove(update_order, 1)
 			local num = update_pending[name]
@@ -49,9 +51,30 @@ function events.tick()
 			if num ~= nil then
 				pings.innums(name, num)
 				update_pending[name] = nil
-
 				sent = sent + 1
 			end
+		end
+	end
+
+	if t % full_sync_interval == 0 then
+		local keys = {}
+
+		for name, _ in pairs(nums) do
+			table.insert(keys, name)
+		end
+
+		table.sort(keys)
+
+		while full_sync_index <= #keys and sent < send_limit do
+			local name = keys[full_sync_index]
+			pings.innums(name, nums[name])
+
+			full_sync_index = full_sync_index + 1
+			sent = sent + 1
+		end
+
+		if full_sync_index > #keys then
+			full_sync_index = 1
 		end
 	end
 
